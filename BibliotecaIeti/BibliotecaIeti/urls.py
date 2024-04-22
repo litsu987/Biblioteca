87443579ab747af4d4e324901fc4fb167514c132
root <root@biblio1.ieti.local>
2024-04-22 14:09:02 +0000

    Config bd

--- BibliotecaIeti/BibliotecaIeti/urls.py
+++ BibliotecaIeti/BibliotecaIeti/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.views.decorators.csrf import csrf_exempt
+
+admin.site.login = csrf_exempt(admin.site.login)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
